7a91bcf9e23673193d52a2a081d0b6ac7fe9671e
Aris Mateo <arismariamateo@gmail.com>
2025-05-02 10:38:59 -0500

    Agregar multiplicación y división

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,21 @@
 function sumar(a, b) {
     return a + b;
-  }
+}
   
 function restar(a, b) {
     return a - b;
-  }
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+  
+  function dividir(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
+}
 
   sumar(5,3);
   restar(10,4);
+
+  
